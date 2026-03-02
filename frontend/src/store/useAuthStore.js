79e18c19e70f7181d11f75a14d95c0f7325d17cd
aryannema <aaryan.techy@gmail.com>
2026-03-02 23:29:10 +0530

    implemented Profile page

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,6 +1,6 @@
 import { create } from "zustand";
 import { axiosInstance } from "../lib/axios";
-import toast from "react-hot-toast";
+import toast, { ToastBar } from "react-hot-toast";
 
 export const useAuthStore = create((set) => ({
   authUser: null,
@@ -35,20 +35,18 @@ export const useAuthStore = create((set) => ({
     }
   },
 
-  login: async(data) => {
-    set({isLoggingIn:true})
+  login: async (data) => {
+    set({ isLoggingIn: true });
     try {
-      const res = await axiosInstance.post("/auth/login", data)
-      set({authUser:res.data})
-      toast.success("Logged in successfully")
+      const res = await axiosInstance.post("/auth/login", data);
+      set({ authUser: res.data });
+      toast.success("Logged in successfully");
     } catch (error) {
-      toast.error(error.response.data.message)
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isLoggingIn: false });
     }
-
-    finally {
-      set({isLoggingIn:false})
-    }
-  }
+  },
 
   logout: async () => {
     try {
@@ -57,6 +55,21 @@ export const useAuthStore = create((set) => ({
       toast.success("Logged out successfully");
     } catch (error) {
       toast.error(error.response.data.message);
+    }
+  },
+
+  updateProfile: async (data) => {
+    set({ isUpdatingProfile: true });
+
+    try {
+      const res = await axiosInstance.put("/auth/update-profile", data);
+      set({ authUser: res.data });
+      toast.success("Profile updated successfully");
+    } catch (error) {
+      console.log("Error in update profile");
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isUpdatingProfile: false });
     }
   },
 }));

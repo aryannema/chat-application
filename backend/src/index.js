c9556f32946ec2b777e522b6a0ef6574e0bbe01a
aryannema <aaryan.techy@gmail.com>
2026-03-02 11:29:18 +0530

    changes made to backend files and initialized setup files for frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import messageRoutes from "./routes/message.route.js";
 import dotenv from "dotenv";
 import { connectdb } from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
@@ -11,6 +12,12 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://locahost:5173",
+    credentials: true,
+  }),
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);

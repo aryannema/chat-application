d767b5ef6baf8d5f614ce972d11ea99da7ecee8b
aryannema <aaryan.techy@gmail.com>
2026-03-03 11:57:42 +0530

    added socket.io functionality in both backend and frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,9 +5,10 @@ import dotenv from "dotenv";
 import { connectdb } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import { app, server } from "./lib/socket.js";
 
 dotenv.config();
-const app = express();
+
 const PORT = process.env.PORT;
 
 app.use(express.json());
@@ -22,7 +23,7 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log("Server is running on port 5001");
   connectdb();
 });
